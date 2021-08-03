601bf0ffd3fd7aa1fbe180dde1df240e29c6e50e
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-08-03 10:11:14 +0530

    🛠 created base and web packages

---
+++ environments/base/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ *  Contains JavaScript utility functions that can be run independent
+ *  of the environment.
+ *  Created On 03 August 2021
+ */
+
+export default async () => {
+    console.log('environment independent base')
+}
---
+++ environments/web/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ *  Contains JavaScript utility functions that are suitable
+ *  to be run inside a web browser.
+ *  Created On 03 August 2021
+ */
+
+export default async () => {
+    console.log('utility functions for a browser environment')
+}
